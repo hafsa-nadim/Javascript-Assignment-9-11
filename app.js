7c86941ec2aa733cd4adb71e792b736883ecf6dc
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-05-16 01:00:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+      
+// Question no:01
+
+// var city = prompt("Enter Your city name:");
+
+// if (city === "karachi"|| city==="Karachi") {
+//   document.write("Welcome to the city of lights");
+// }
+
+// Question no:02
+
+// var gender = prompt("Enter your gender: ");
+ 
+// if (gender==="male"||gender==="Male"){
+//     document.write("Good Morning Sir");
+// }
+
+// else if(gender==="female"||gender==="Female"){
+//     document.write("Good Morning Ma'am");
+// }
+
+// else{
+//     document.write("Invalid gender!");
+// }
+
+// Question no:03
+
+// let signal = prompt("Enter Signal Color");
+// if(signal==="red"){
+//     document.write("Must stop");
+// }
+// else if(signal==="yellow"){
+//     document.write("Ready to move");
+// }
+// else if(signal==="green"){
+//     document.write("Move now");
+// }
+// else{
+//     document.write("Enter color properly");
+
+// }
+
+// Question no:04
+
+// let fuel = prompt("Enter fuel in liters");
+// if (fuel<0.25){
+//     document.write("Please refill the fuel in your car");
+// }
+// else{
+//     document.write("No need to refill your car!");
+// }
+
+// Question no:05
+
+// run in the browser to check the output 
+
+// Question no:06
+
+// let obtMark1 = +prompt("Enter marks of subject 1")
+// let obtMark2 = +prompt("Enter marks of subject 2")
+// let obtMark3 = +prompt("Enter marks of subject 3")
+
+// let totalMark1 = +prompt("Enter total marks of sub 1")
+// let totalMark2 = +prompt("Enter total marks of sub 2")
+// let totalMark3 = +prompt("Enter total marks of sub 3")
+
+// let addTotalMark = totalMark1 + totalMark2 + totalMark3;
+// let addObtMark = obtMark1 + obtMark2 + obtMark3;
+// let per = (addObtMark*100)/addTotalMark;
+// document.write(per)
+
+// document.write("<h1> Mark sheet </h1>");
+// document.write("Total marks: " , addTotalMark, "<br>");
+// document.write("Obtained marks: " , addObtMark, "<br>");
+// document.write("Percentage: " ,per , "% <br>");
+
+// if(per <=100 && per >=80){
+//     document.write("Grade: " ,A-one , "<br>");
+//     document.write("Remarks: " ,excellent , "<br>");
+// }
+// else if(per <80 && per >=70){
+//     document.write("Grade: " ,A , "<br>");
+//     document.write("Remarks: " ,good , "<br>");
+// }
+// else if(per >=60){
+//     document.write("Grade: " ,B , "<br>");
+//     document.write("Remarks: " ,"need to improve" , "<br>");
+// }
+
+// else {
+//     document.write("Grade: " ,F , "<br>");
+//     document.write("Remarks: " ,sorry , "<br>");
+// }
+
+// Question no:07
+ 
+// let hide = 16;
+// let number = +prompt("Enter any number");
+// if (number===hide+1||number===hide-1){
+//     document.write("Close enough to the correct answer")
+// }
+
+// else if (number == hide){
+//     document.write("Binggo! correct");
+// }
+
+// else{
+//     document.write("Try again!");
+// }
+
+// Question no:o8
+
+// let num = prompt("Enter any number: ");
+
+// if(num%3===0){
+//     document.write("The number is divisible by 3");
+// }
+// else{
+//     document.write("Its not divisible by 3");
+// }
+
+// Question no:08
+
+// let number=prompt("Enter any number: ");
+// if(number%2===0){
+//     document.write("Number is Even");
+// }
+
+// else if(number%3===0){
+//     document.write("Number is odd");
+// }
+
+//  Question no:09
+
+// let T = +prompt("enter temperature");
+// if(T > 40){
+//     document.write("It is too hot outside.");
+// }
+// else if(T > 30){
+//     document.write("The Weather today is Normal.");
+// }
+// else if(T > 20){
+//     document.write("Today’s Weather is cool.");
+// }
+// else if(T > 10){
+//     document.write("OMG! Today’s weather is so Cool.");
+// }
+// else{
+//     document.write("OMG! Today’s weather is so Cool.");
+// }
+
+// Question no:10
+
+// let value1 = +prompt("Enter First Value");
+// let value2 = +prompt("Enter Second Value");
+// document.write("<h1>********SIMPLE CALCULATOR********<br></h1>")
+// document.write("<h3> ADDITION </h3>", value1 , "  +  ", value2 , "  =  ", value1 + value2, "<br>");
+// document.write("<h3> SUBTRACTION </h3>",value1 , "  -  ", value2 , "  =  ", value1 - value2, "<br>");
+// document.write("<h3> MULTIPLICATION </h3>",value1 , "  *  ", value2 , "  =  ", value1* value2, "<br>");
+// document.write("<h3> DIVISION </h3>",value1 , "  /  ", value2 , "  =  ", value1 / value2, "<br>");
+// document.write("<h3> MODULOUS </h3>",value1 , "  %  ", value2 , "  =  ", value1 % value2, "<br>");
+
+
+
